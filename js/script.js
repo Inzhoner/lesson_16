72638dc3e590c99e6d5e3b370224f25ba3fab8d1
Andrii <mr.lemak1@gmail.com>
2024-05-30 12:14:05 +0300

    change menu-icon js

--- js/script.js
+++ js/script.js
@@ -3,7 +3,7 @@
 document.addEventListener("click", function (e) {
     const targetElement = e.target;
     // .icon-menu це клас кнопки при кліку на яку буде спрацьовувати код додаватись до тегу html клас menu-open 
-    if (targetElement.closest('.icon-menu')) {
+    if (targetElement.closest('.menu-icon')) {
       document.documentElement.classList.toggle('menu-open');
       e.preventDefault();
     }
